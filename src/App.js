5e99e151e2edd37041fef291eb81ab5af2829858
BarbaraCB <71982874+BarbaraCB@users.noreply.github.com>
2021-02-07 15:28:06 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">
-    <h1>Weather App</h1>
-    <footer>
-      Coded by Barbara Bousquet and {" "}
-    
-  <a href="https://github.com/BarbaraCB/WeatherApp-React" target= "_blank">open-sourced on Github
-    </a>
-    </footer>
-    </div>;
+  return (
+  <div className="App">
+    <div className="container">
+      <h1>Weather App</h1>
+      <footer>
+        Coded by Barbara Bousquet and {" "}
+      <a href="https://github.com/BarbaraCB/WeatherApp-React" target= "_blank">open-sourced on Github
+      </a>
+      </footer>
+    </div>
+  </div>
+    );
 }
 
