66afbe6fc7f8f0daa7e665b0fd43e9b4faa5b409
BarbaraCB <71982874+BarbaraCB@users.noreply.github.com>
2021-02-07 14:58:50 -0500

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return <div className="App">Weather App
+  <a href="https://github.com/BarbaraCB/WeatherApp-React" target= "_blank">
+    </a>
+    </div>;
 }
 
