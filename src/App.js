81987ef8de64f87d9132f7043decf364ddbc5072
BarbaraCB <71982874+BarbaraCB@users.noreply.github.com>
2021-02-07 17:56:39 -0500

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
   <div className="App">
     <div className="container">
-      <h1>Weather App</h1>
+      
+      <Weather />
       <footer>
         Coded by Barbara Bousquet and {" "}
       <a href="https://github.com/BarbaraCB/WeatherApp-React" target= "_blank">open-sourced on Github
