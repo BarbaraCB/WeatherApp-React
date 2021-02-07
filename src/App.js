b350c9842f1f2a9579570e9403a4a40d8e4fe097
BarbaraCB <71982874+BarbaraCB@users.noreply.github.com>
2021-02-07 15:09:02 -0500

    Added github url

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App
-  <a href="https://github.com/BarbaraCB/WeatherApp-React" target= "_blank">
+  return <div className="App">
+    <h1>Weather App</h1>
+    <footer>
+      Coded by Barbara Bousquet and {" "}
+    
+  <a href="https://github.com/BarbaraCB/WeatherApp-React" target= "_blank">open-sourced on Github
     </a>
+    </footer>
     </div>;
 }
 
